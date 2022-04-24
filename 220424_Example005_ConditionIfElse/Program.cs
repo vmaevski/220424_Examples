4e07111287a0cbf269efd6b1b0453ce1061234f9
vmaevski <vmaevski@mail.ru>
2022-04-24 09:47:43 +0700

    add _Example005 ConditionIfElse

---
+++ 220424_Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower()  == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
