ad74a971f2e27155018ce4480eaf8adb7748a6d8
vmaevski <vmaevski@mail.ru>
2022-04-24 13:11:06 +0700

    add Example 008 Friends-n-dog

---
+++ 220424_Example008_Friends-n-dog/Program.cs
@@ -0,0 +1,21 @@
+﻿double count=0, distance = 10000, friend1Speed = 1, friend2Speed = 2, dogSpeed = 5, friend = 2;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (friend2Speed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (friend1Speed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (friend1Speed + friend2Speed) * time;
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз.");
