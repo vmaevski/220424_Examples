c69ed93a92b589da1533f3e1d0e5af02a01abb97
vmaevski <vmaevski@mail.ru>
2022-05-18 17:24:44 +0700

    Add Replace letter

---
+++ 220517_Replace_Letters/Program.cs
@@ -0,0 +1,20 @@
+﻿// Заменить в тексте ПРОБЕЛ на  "|", с на С
+
+string text = "В лесу родилась Ёлочка, В лесу она росла.";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++) 
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;    
+}
+
+string NewText = Replace(text, ' ', '|');
+NewText = Replace(NewText, 'с', 'С');
+Console.WriteLine(NewText);
+
