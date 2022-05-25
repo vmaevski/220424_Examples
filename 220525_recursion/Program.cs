7d7953daee7e7ac9475e67bcddaefa524fa9eba8
vmaevski <vmaevski@mail.ru>
2022-05-26 06:56:15 +0700

    Add fibonacci

--- 220525_recursion/Program.cs
+++ 220525_recursion/Program.cs
@@ -40,7 +40,25 @@
 double Factorial(int n)
 {
     //if (n == 0 || n == 1) return 1;
-    if (n == 1) return 1;
+    if (n == 0 || n == 1) return 1;
     else return n * Factorial(n - 1);
 }
-Console.WriteLine(Factorial(20));
+//Console.WriteLine(Factorial(20));
+
+double Fibonacci(int n)
+{
+    if (n==1 || n==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i} {Fibonacci(i)}");
+}
+    
